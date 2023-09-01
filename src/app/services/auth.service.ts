e5039856cc40e6aad638db4438303c2248143157
Narcizo <narcizo.gabriel2@gmail.com>
2023-09-01 18:00:58 -0300

    Tela de login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private auth: Auth) { }
+
+  async register({email, password}: {email:string, password:string}){
+    try{
+      return await createUserWithEmailAndPassword(
+        this.auth,
+        email, 
+        password);
+    }
+    catch(e){
+      return null;
+    }
+  }
+
+  async login({email, password}: {email:string, password:string}){
+    try{
+      return await signInWithEmailAndPassword(
+        this.auth,
+        email, 
+        password);
+    }
+    catch(e){
+      return null;
+    }
+  }
+
+  logout(){
+    return signOut(this.auth)
+  }
 }
